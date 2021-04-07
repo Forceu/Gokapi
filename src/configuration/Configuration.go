Forceu/Gokapi

commit ab4edaff949970d599e0531e5458915494a24184

Added env variables, major refactoring

src/configuration/Configuration.go
@@ -1,0 +1,316 @@
+package configuration
+
+/**
+Loading and saving of the persistent configuration
+*/
+
+import (
+	"Gokapi/src/environment"
+	"Gokapi/src/helper"
+	"Gokapi/src/storage/filestructure"
+	"Gokapi/src/webserver/sessionstructure"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/crypto/ssh/terminal"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+// Default port that the program runs on
+const defaultPort = "53842"
+
+// Min length of admin password in characters
+const minLengthPassword = 6
+
+// Object containing the environment
+var Environment environment.Environment
+
+// Object containing the configuration
+var ServerSettings Configuration
+
+// Version of the configuration structure. Used for upgrading
+const currentConfigVersion = 2
+
+// Struct that contains the global configuration
+type Configuration struct {
+	Port             string                              `json:"Port"`
+	AdminName        string                              `json:"AdminName"`
+	AdminPassword    string                              `json:"AdminPassword"`
+	ServerUrl        string                              `json:"ServerUrl"`
+	DefaultDownloads int                                 `json:"DefaultDownloads"`
+	DefaultExpiry    int                                 `json:"DefaultExpiry"`
+	DefaultPassword  string                              `json:"DefaultPassword"`
+	RedirectUrl      string                              `json:"RedirectUrl"`
+	Sessions         map[string]sessionstructure.Session `json:"Sessions"`
+	Files            map[string]filestructure.File       `json:"Files"`
+	ConfigVersion    int                                 `json:"ConfigVersion"`
+	SaltAdmin        string                              `json:"SaltAdmin"`
+	SaltFiles        string                              `json:"SaltFiles"`
+	LengthId         int                                 `json:"LengthId"`
+}
+
+// Loads the configuration or creates the folder structure and a default configuration
+func Load() {
+	Environment = environment.New()
+	helper.CreateConfigDir(Environment.ConfigDir)
+	helper.CreateDataDir(Environment.DataDir)
+	if !helper.FileExists(Environment.ConfigPath) {
+		generateDefaultConfig()
+	}
+	file, err := os.Open(Environment.ConfigPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	ServerSettings = Configuration{}
+	err = decoder.Decode(&ServerSettings)
+	if err != nil {
+		log.Fatal(err)
+	}
+	updateConfig()
+}
+
+// Upgrades the ServerSettings if saved with a previous version
+func updateConfig() {
+	// < v1.1.2
+	if ServerSettings.ConfigVersion < 2 {
+		ServerSettings.SaltAdmin = "eefwkjqweduiotbrkl##$2342brerlk2321"
+		ServerSettings.SaltFiles = "P1UI5sRNDwuBgOvOYhNsmucZ2pqo4KEvOoqqbpdu"
+		ServerSettings.LengthId = 15
+	}
+
+	if ServerSettings.ConfigVersion < currentConfigVersion {
+		fmt.Println("Successfully upgraded database")
+		ServerSettings.ConfigVersion = currentConfigVersion
+		Save()
+	}
+}
+
+// Creates a default configuration and asks for items like username/password etc.
+func generateDefaultConfig() {
+	fmt.Println("First start, creating new admin account")
+	username := askForUsername()
+	password := askForPassword()
+	port := askForPort()
+	url := askForUrl(port)
+	redirect := askForRedirect()
+	localOnly := askForLocalOnly()
+	bindAddress := "127.0.0.1:" + port
+	if localOnly == environment.IsFalse {
+		bindAddress = ":" + port
+	}
+
+	ServerSettings = Configuration{
+		Port:             bindAddress,
+		AdminName:        username,
+		AdminPassword:    HashPassword(password, false),
+		ServerUrl:        addTrailingSlash(url),
+		DefaultDownloads: 1,
+		DefaultExpiry:    14,
+		RedirectUrl:      redirect,
+		Files:            make(map[string]filestructure.File),
+		Sessions:         make(map[string]sessionstructure.Session),
+		ConfigVersion:    currentConfigVersion,
+		SaltAdmin:        Environment.SaltAdmin,
+		SaltFiles:        Environment.SaltFiles,
+	}
+	Save()
+}
+
+// Saves the configuration as a json file
+func Save() {
+	file, err := os.OpenFile(Environment.ConfigPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("Error reading configuration:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(&ServerSettings)
+	if err != nil {
+		fmt.Println("Error writing configuration:", err)
+		os.Exit(1)
+	}
+}
+
+// Asks for username or loads it from env and returns input as string if valid
+func askForUsername() string {
+	fmt.Print("Username: ")
+	envUsername := Environment.AdminName
+	if envUsername != "" {
+		fmt.Println(envUsername)
+		return envUsername
+	}
+	username := helper.ReadLine()
+	if len(username) >= 4 {
+		return username
+	}
+	fmt.Println("Username needs to be at least 4 characters long")
+	return askForUsername()
+}
+
+// Asks for password or loads it from env and returns input as string if valid
+func askForPassword() string {
+	fmt.Print("Password: ")
+	envPassword := Environment.AdminPassword
+	if envPassword != "" {
+		fmt.Println("*******************")
+		if utf8.RuneCountInString(envPassword) < minLengthPassword {
+			fmt.Println("\nPassword needs to be at least " + strconv.Itoa(minLengthPassword) + " characters long")
+			os.Exit(1)
+		}
+		return envPassword
+	}
+	password1, err := terminal.ReadPassword(0)
+	helper.Check(err)
+	if utf8.RuneCountInString(string(password1)) < minLengthPassword {
+		fmt.Println("\nPassword needs to be at least " + strconv.Itoa(minLengthPassword) + " characters long")
+		return askForPassword()
+	}
+	fmt.Print("\nPassword (repeat): ")
+	password2, err := terminal.ReadPassword(0)
+	helper.Check(err)
+	if string(password1) != string(password2) {
+		fmt.Println("\nPasswords dont match")
+		return askForPassword()
+	}
+	fmt.Println()
+	return string(password1)
+}
+
+// Asks if the server shall be bound to 127.0.0.1 or loads it from env and returns result as bool
+func askForLocalOnly() string {
+	if environment.IsDocker != "false" {
+		return environment.IsTrue
+	}
+	fmt.Print("Bind port to localhost only? [Y/n]: ")
+	envLocalhost := Environment.WebserverLocalhost
+	if envLocalhost != "" {
+		fmt.Println(envLocalhost)
+		return envLocalhost
+	}
+	input := strings.ToLower(helper.ReadLine())
+	if input == "n" || input == "no" {
+		return environment.IsFalse
+	}
+	return environment.IsTrue
+}
+
+// Asks for server port or loads it from env and returns input as string if valid
+func askForPort() string {
+	fmt.Print("Server Port [" + defaultPort + "]: ")
+	envPort := Environment.WebserverPort
+	if envPort != "" {
+		fmt.Println(envPort)
+		return envPort
+	}
+	port := helper.ReadLine()
+	if port == "" {
+		return defaultPort
+	}
+	if !isValidPortNumber(port) {
+		return askForPort()
+	}
+	return port
+}
+
+// Asks for server URL or loads it from env and returns input as string if valid
+func askForUrl(port string) string {
+	fmt.Print("External Server URL [http://127.0.0.1:" + port + "/]: ")
+	envUrl := Environment.ExternalUrl
+	if envUrl != "" {
+		fmt.Println(envUrl)
+		if !isValidUrl(envUrl) {
+			os.Exit(1)
+		}
+		return envUrl
+	}
+	url := helper.ReadLine()
+	if url == "" {
+		return "http://127.0.0.1:" + port + "/"
+	}
+	if !isValidUrl(url) {
+		return askForUrl(port)
+	}
+	return url
+}
+
+// Asks for redirect URL or loads it from env and returns input as string if valid
+func askForRedirect() string {
+	fmt.Print("URL that the index gets redirected to [https://github.com/Forceu/Gokapi/]: ")
+	envRedirect := Environment.RedirectUrl
+	if envRedirect != "" {
+		fmt.Println(envRedirect)
+		if !isValidUrl(envRedirect) {
+			os.Exit(1)
+		}
+		return envRedirect
+	}
+	url := helper.ReadLine()
+	if url == "" {
+		return "https://github.com/Forceu/Gokapi/"
+	}
+	if !isValidUrl(url) {
+		return askForRedirect()
+	}
+	return url
+}
+
+// Returns true if URL starts with http:// or https://
+func isValidUrl(url string) bool {
+	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+		fmt.Println("URL needs to start with http:// or https://")
+		return false
+	}
+	postfix := strings.Replace(url, "http://", "", -1)
+	postfix = strings.Replace(postfix, "https://", "", -1)
+	return len(postfix) > 0
+}
+
+// Checks if the string is a valid port number
+func isValidPortNumber(input string) bool {
+	port, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Input needs to be a number")
+		return false
+	}
+	if port < 0 || port > 65353 {
+		fmt.Println("Port needs to be between 0-65353")
+		return false
+	}
+	return true
+}
+
+// Adds a / character to the end of an URL if it does not exist
+func addTrailingSlash(url string) string {
+	if !strings.HasSuffix(url, "/") {
+		return url + "/"
+	}
+	return url
+}
+
+func DisplayPasswordReset() {
+	ServerSettings.AdminPassword = HashPassword(askForPassword(), false)
+	Save()
+}
+
+// Hashes a password with SHA256 and a salt
+func HashPassword(password string, useFileSalt bool) string {
+	if password == "" {
+		return ""
+	}
+	salt := Environment.SaltAdmin
+	if useFileSalt {
+		salt = Environment.SaltFiles
+	}
+	bytes := []byte(password + salt)
+	hash := sha1.New()
+	hash.Write(bytes)
+	return hex.EncodeToString(hash.Sum(nil))
+}

Configuration.go
@@ -1,185 +1,0 @@
-package main
-
-/**
-Loading and saving of the persistent configuration
-*/
-
-import (
-	"encoding/json"
-	"fmt"
-	"golang.org/x/crypto/ssh/terminal"
-	"log"
-	"os"
-	"strings"
-)
-
-
-// Name of the config dir that will be created
-const configDir = "config"
-
-// Name of the config file where the configuration is written to
-const configFile = "config.json"
-
-// Full path of configDir and configFile
-const configPath = configDir + "/" + configFile
-
-// Name of the data dir that will be created
-const dataDir = "data"
-
-// Global object containing the configuration
-var globalConfig Configuration
-
-// Struct that contains the global configuration
-type Configuration struct {
-	Port             string              `json:"Port"`
-	AdminName        string              `json:"AdminName"`
-	AdminPassword    string              `json:"AdminPassword"`
-	ServerUrl        string              `json:"ServerUrl"`
-	DefaultDownloads int                 `json:"DefaultDownloads"`
-	DefaultExpiry    int                 `json:"DefaultExpiry"`
-	DefaultPassword  string              `json:"DefaultPassword"`
-	RedirectUrl      string              `json:"RedirectUrl"`
-	Sessions         map[string]Session  `json:"Sessions"`
-	Files            map[string]FileList `json:"Files"`
-}
-
-// Loads the configuration or creates the folder structure and a default configuration
-func loadConfig() {
-	createConfigDir()
-	if !fileExists(configPath) {
-		generateDefaultConfig()
-	}
-	file, err := os.Open(configPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-	globalConfig = Configuration{}
-	err = decoder.Decode(&globalConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-// Creates a default configuration and asks for items like username/password etc.
-func generateDefaultConfig() {
-	fmt.Println("First start, creating new admin account")
-	username := askForUsername()
-	password := askForPassword()
-	url := askForUrl()
-	redirect := askForRedirect()
-	localOnly := askForLocalOnly()
-	port := "127.0.0.1:53842"
-	if !localOnly {
-		port = "0.0.0.0:53842"
-	}
-
-	globalConfig = Configuration{
-		Port:             port,
-		AdminName:        username,
-		AdminPassword:    hashPassword(password, SALT_PW_ADMIN),
-		ServerUrl:        url,
-		DefaultDownloads: 1,
-		DefaultExpiry:    14,
-		RedirectUrl:      redirect,
-		Files:            make(map[string]FileList),
-		Sessions:         make(map[string]Session),
-	}
-	saveConfig()
-}
-
-// Saves the configuration as a json file
-func saveConfig() {
-	file, err := os.OpenFile(configPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		fmt.Println("Error reading configuration:", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-	encoder := json.NewEncoder(file)
-	err = encoder.Encode(&globalConfig)
-	if err != nil {
-		fmt.Println("Error writing configuration:", err)
-		os.Exit(1)
-	}
-}
-
-// Asks for username and returns input as string if valid
-func askForUsername() string {
-	fmt.Print("Username: ")
-	username := readLine()
-	if len(username) >= 4 {
-		return username
-	}
-	fmt.Println("Username needs to be at least 4 characters long")
-	return askForUsername()
-}
-
-// Asks for password and returns input as string if valid
-func askForPassword() string {
-	fmt.Print("Password: ")
-	password1, err := terminal.ReadPassword(0)
-	check(err)
-	if len(password1) < 6 {
-		fmt.Println("\nPassword needs to be at least 6 characters long")
-		return askForPassword()
-	}
-	fmt.Print("\nPassword (repeat): ")
-	password2, err := terminal.ReadPassword(0)
-	check(err)
-	if string(password1) != string(password2) {
-		fmt.Println("\nPasswords dont match")
-		return askForPassword()
-	}
-	fmt.Println()
-	return string(password1)
-}
-
-// Asks if the server shall be bound to 127.0.0.1 and returns result as bool
-func askForLocalOnly() bool {
-	if IS_DOCKER != "false" {
-		return false
-	}
-	fmt.Print("Bind port to localhost only? [Y/n]: ")
-	input := strings.ToLower(readLine())
-	return input != "n"
-}
-
-// Asks for server URL and returns input as string if valid
-func askForUrl() string {
-	fmt.Print("Server URL [eg. https://gokapi.url/]: ")
-	url := readLine()
-	if !isValidUrl(url) {
-		return askForUrl()
-	}
-	if !strings.HasSuffix(url, "/") {
-		url = url + "/"
-	}
-	return url
-}
-
-// Asks for redirect URL and returns input as string if valid
-func askForRedirect() string {
-	fmt.Print("URL that the index gets redirected to [eg. https://yourcompany.com/]: ")
-	url := readLine()
-	if url == "" {
-		return "https://github.com/Forceu/Gokapi/"
-	}
-	if !isValidUrl(url) {
-		return askForUrl()
-	}
-	if !strings.HasSuffix(url, "/") {
-		url = url + "/"
-	}
-	return url
-}
-
-// Returns true if URL starts with http:// or https://
-func isValidUrl(url string) bool {
-	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
-		fmt.Println("URL needs to start with http:// or https://")
-		return false
-	}
-	return true
-}

.release/entrypoint.sh
@@ -16,7 +16,7 @@ for target in $targets; do
   fi
 
   echo "----> Building project for: $target"
-  GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -ldflags="-s -w -X 'main.BUILDER=Github Release Builder' -X 'main.BUILD_TIME=$(date)'" -o $output
+  GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -ldflags="-s -w -X 'environment.Builder=Github Release Builder' -X 'environment.BuildTime=$(date)'" -o $output
   zip -j $output.zip $output > /dev/null
   rm $output
 done

Dockerfile
@@ -10,7 +10,7 @@ RUN cd /compile && go mod download
   
 COPY . /compile  
 
-RUN cd /compile && CGO_ENABLED=0 go build -ldflags="-s -w -X 'main.IS_DOCKER=true' -X 'main.BUILDER=Project Docker File' -X 'main.BUILD_TIME=$(date)'" -o /compile/gokapi
+RUN cd /compile && CGO_ENABLED=0 go build -ldflags="-s -w -X 'environment.IsDocker=true' -X 'environment.Builder=Project Docker File' -X 'environment.BuildTime=$(date)'" -o /compile/gokapi
 
 FROM alpine:3.13
 

FileServing.go
@@ -1,127 +1,0 @@
-package main
-
-/**
-Serving and processing uploaded files
-*/
-
-import (
-	"crypto/sha1"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"mime/multipart"
-	"os"
-	"time"
-)
-
-
-// The length for IDs used in URLs. Can be increased to improve security and decreased to increase readability
-const lengthId = 15
-
-// Struct used for saving information about an uploaded file
-type FileList struct {
-	Id                 string `json:"Id"`
-	Name               string `json:"Name"`
-	Size               string `json:"Size"`
-	SHA256             string `json:"SHA256"`
-	ExpireAt           int64  `json:"ExpireAt"`
-	ExpireAtString     string `json:"ExpireAtString"`
-	DownloadsRemaining int    `json:"DownloadsRemaining"`
-	PasswordHash       string `json:"PasswordHash"`
-}
-
-// Converts the file info to a json String used for returning a result for an upload
-func (f *FileList) toJsonResult() string {
-	result := Result{
-		Result:   "OK",
-		Url:      globalConfig.ServerUrl + "d?id=",
-		FileInfo: f,
-	}
-	bytes, err := json.Marshal(result)
-	if err != nil {
-		fmt.Println(err)
-		return "{\"Result\":\"error\",\"ErrorMessage\":\"" + err.Error() + "\"}"
-	}
-	return string(bytes)
-}
-
-// The struct used for the result after an upload
-type Result struct {
-	Result   string    `json:"Result"`
-	FileInfo *FileList `json:"FileInfo"`
-	Url      string    `json:"Url"`
-}
-
-// Creates a new file in the system. Called after an upload has been completed. If a file with the same sha256 hash
-// already exists, it is deduplicated. This function gathers information about the file, creates and ID and saves
-// it into the global configuration.
-func createNewFile(fileContent *multipart.File, fileHeader *multipart.FileHeader, expireAt int64, downloads int, password string) (FileList, error) {
-	id, err := generateRandomString(lengthId)
-	if err != nil {
-		id = unsafeId(lengthId)
-	}
-
-	fileBytes, err := ioutil.ReadAll(*fileContent)
-	if err != nil {
-		return FileList{}, err
-	}
-	hash := sha1.New()
-	hash.Write(fileBytes)
-	file := FileList{
-		Id:                 id,
-		Name:               fileHeader.Filename,
-		SHA256:             hex.EncodeToString(hash.Sum(nil)),
-		Size:               byteCountSI(fileHeader.Size),
-		ExpireAt:           expireAt,
-		ExpireAtString:     time.Unix(expireAt, 0).Format("2006-01-02 15:04"),
-		DownloadsRemaining: downloads,
-		PasswordHash:       hashPassword(password, SALT_PW_FILES),
-	}
-	globalConfig.Files[id] = file
-	filename := dataDir + "/" + file.SHA256
-	if !fileExists(dataDir + "/" + file.SHA256) {
-		destinationFile, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
-		if err != nil {
-			return FileList{}, err
-		}
-		defer destinationFile.Close()
-		destinationFile.Write(fileBytes)
-	}
-	saveConfig()
-	return file, nil
-}
-
-// Removes expired files from the config and from the filesystem if they are not referenced by other files anymore
-// Will be called periodically or after a file has been manually deleted in the admin view.
-// If parameter periodic is true, this function is recursive and calls itself every hour.
-func cleanUpOldFiles(periodic bool) {
-	timeNow := time.Now().Unix()
-	wasItemDeleted := false
-	for key, element := range globalConfig.Files {
-		if element.ExpireAt < timeNow || element.DownloadsRemaining < 1 {
-			deleteFile := true
-			for _, secondLoopElement := range globalConfig.Files {
-				if element.Id != secondLoopElement.Id && element.SHA256 == secondLoopElement.SHA256 {
-					deleteFile = false
-				}
-			}
-			if deleteFile {
-				err := os.Remove(dataDir + "/" + element.SHA256)
-				if err != nil {
-					fmt.Println(err)
-				}
-			}
-			delete(globalConfig.Files, key)
-			wasItemDeleted = true
-		}
-	}
-	if wasItemDeleted {
-		saveConfig()
-		cleanUpOldFiles(false)
-	}
-	if periodic {
-		time.Sleep(time.Hour)
-		go cleanUpOldFiles(periodic)
-	}
-}

Main.go
@@ -5,33 +5,30 @@ Main routine
 */
 
 import (
+	"Gokapi/src/configuration"
+	"Gokapi/src/environment"
+	"Gokapi/src/storage"
+	"Gokapi/src/webserver"
+	"embed"
 	"fmt"
 	"math/rand"
 	"os"
 	"time"
 )
 
-
 // needs to be changed in ./templates/string_constants.tmpl as well
-const VERSION = "1.1.0"
-
-// Salt for the admin password hash
-const SALT_PW_ADMIN = "eefwkjqweduiotbrkl##$2342brerlk2321"
-
-// Salt for the file password hashes
-const SALT_PW_FILES = "P1UI5sRNDwuBgOvOYhNsmucZ2pqo4KEvOoqqbpdu"
+const VERSION = "1.1.2"
 
 // Main routine that is called on startup
 func main() {
 	checkPrimaryArguments()
 	rand.Seed(time.Now().UnixNano())
+	fmt.Println(logo)
 	fmt.Println("Gokapi v" + VERSION + " starting")
-	createDataDir()
-	loadConfig()
+	configuration.Load()
 	checkArguments()
-	initTemplates()
-	go cleanUpOldFiles(true)
-	startWebserver()
+	go storage.CleanUp(true)
+	webserver.Start(staticFolderEmbedded, templateFolderEmbedded)
 }
 
 // Checks for command line arguments that have to be parsed before loading the configuration
@@ -39,9 +36,9 @@ func checkPrimaryArguments() {
 	if len(os.Args) > 1 {
 		if os.Args[1] == "--version" || os.Args[1] == "-v" {
 			fmt.Println("Gokapi v" + VERSION)
-			fmt.Println("Builder: " + BUILDER)
-			fmt.Println("Build Date: " + BUILD_TIME)
-			fmt.Println("Docker Version: " + IS_DOCKER)
+			fmt.Println("Builder: " + environment.Builder)
+			fmt.Println("Build Date: " + environment.BuildTime)
+			fmt.Println("Docker Version: " + environment.IsDocker)
 			os.Exit(0)
 		}
 	}
@@ -52,8 +49,25 @@ func checkArguments() {
 	if len(os.Args) > 1 {
 		if os.Args[1] == "--reset-pw" {
 			fmt.Println("Password change requested")
-			globalConfig.AdminPassword = hashPassword(askForPassword(), SALT_PW_ADMIN)
-			saveConfig()
+			configuration.DisplayPasswordReset()
 		}
 	}
 }
+
+// ASCII art logo
+const logo = ` ██████   ██████  ██   ██  █████  ██████  ██ 
+██       ██    ██ ██  ██  ██   ██ ██   ██ ██ 
+██   ███ ██    ██ █████   ███████ ██████  ██ 
+██    ██ ██    ██ ██  ██  ██   ██ ██      ██ 
+ ██████   ██████  ██   ██ ██   ██ ██      ██ 
+                                             `
+
+// Embedded version of the "static" folder
+// This contains JS files, CSS, images etc
+//go:embed static
+var staticFolderEmbedded embed.FS
+
+// Embedded version of the "templates" folder
+// This contains templates that Gokapi uses for creating the HTML output
+//go:embed templates
+var templateFolderEmbedded embed.FS

commit 70cfe7997a0e578c9c4afaa690148397fc6fd152

Fixed bug that salt was not used for password hashing, introduced in ab4edaff949970d599e0531e5458915494a24184

src/configuration/Configuration.go
@@ -322,9 +322,9 @@ func HashPassword(password string, useFileSalt bool) string {
 	if password == "" {
 		return ""
 	}
-	salt := Environment.SaltAdmin
+	salt := ServerSettings.SaltAdmin
 	if useFileSalt {
-		salt = Environment.SaltFiles
+		salt = ServerSettings.SaltFiles
 	}
 	bytes := []byte(password + salt)
 	hash := sha1.New()