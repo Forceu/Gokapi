a537d7fec44a22400b8e3a1a3f9715d409c1953e
Marc Ole Bulling <Marc-Ole@gmx.de>
2022-08-11 13:44:52 +0200

    Remove building noaws version

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -8,21 +8,19 @@ cd /usr/src/myapp
 go generate ./...
 
 for target in $targets; do
-	for tag in "full" "noaws"; do
-		os="$(echo $target | cut -d '/' -f1)"
-		arch="$(echo $target | cut -d '/' -f2)"
-		output="build/gokapi_${tag}-${os}_${arch}"
-		if [ $os = "windows" ]; then
-			output+='.exe'
-		fi
+	os="$(echo $target | cut -d '/' -f1)"
+	arch="$(echo $target | cut -d '/' -f2)"
+	output="build/gokapi-${os}_${arch}"
+	if [ $os = "windows" ]; then
+		output+='.exe'
+	fi
 
-		echo "----> Building Gokapi ($tag) for $target"
-		GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -tags $tag -ldflags="-s -w -X 'github.com/forceu/gokapi/internal/environment.Builder=Github Release Builder' -X 'github.com/forceu/gokapi/internal/environment.BuildTime=$(date)'" -o $output github.com/forceu/gokapi/cmd/gokapi
-		zip -j $output.zip $output >/dev/null
-		rm $output
-	done
+	echo "----> Building Gokapi for $target"
+	GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -ldflags="-s -w -X 'github.com/forceu/gokapi/internal/environment.Builder=Github Release Builder' -X 'github.com/forceu/gokapi/internal/environment.BuildTime=$(date)'" -o $output github.com/forceu/gokapi/cmd/gokapi
+	zip -j $output.zip $output >/dev/null
+	rm $output
 done
 
 echo "----> Build is complete. List of files at $release_path:"
 cd build/
-ls -l gokapi_*
+ls -l gokapi-*
