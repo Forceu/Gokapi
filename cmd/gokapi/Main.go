1e75b9641dc0369dd61d040b71992b03adb9d0e6
Marc Ole Bulling <Marc-Ole@gmx.de>
2023-04-14 19:42:26 +0200

    Updated version number

--- cmd/gokapi/Main.go
+++ cmd/gokapi/Main.go
@@ -30,7 +30,7 @@ import (
 
 // versionGokapi is the current version in readable form.
 // Other version numbers can be modified in /build/go-generate/updateVersionNumbers.go
-const versionGokapi = "1.7.0"
+const versionGokapi = "1.7.1"
 
 // The following calls update the version numbers, update documentation, minify Js/CSS and build the WASM modules
 //go:generate go run "../../build/go-generate/updateVersionNumbers.go"
