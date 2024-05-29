861388617f19d0cb0b62ec7eccbf16fae7d8dab6
Marc Ole Bulling <Marc-Ole@gmx.de>
2024-05-29 13:31:52 +0200

    Only run Docker container as non-root application if ENV DOCKER_NONROOT is passed #167

--- dockerentry.sh
+++ dockerentry.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-echo "Setting permissions" && \
-chown -R gokapi:gokapi /app && \
-chmod -R 700 /app && \
-echo "Starting application" && \
-exec su-exec gokapi:gokapi /app/gokapi "$@"
+if [ -z "$DOCKER_NONROOT" ]; then
+	exec /app/gokapi "$@"
+else
+	echo "Setting permissions" && \
+	chown -R gokapi:gokapi /app && \
+	chmod -R 700 /app && \
+	echo "Starting application" && \
+	exec su-exec gokapi:gokapi /app/gokapi "$@"
+fi
+
