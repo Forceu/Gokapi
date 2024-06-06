ca63110875e9638109c4ddb61734569e0ca77c4a
Marc Ole Bulling <Marc-Ole@gmx.de>
2024-06-06 21:45:22 +0200

    Modified dropzone library to limit parallel uploads, enabled parallel chunk uploads, set chunksize to 39MB, fixes slow upload #162

--- internal/webserver/web/static/js/admin.js
+++ internal/webserver/web/static/js/admin.js
@@ -35,7 +35,7 @@ Dropzone.options.uploaddropzone = {
         // Error handling for chunk upload, especially returning 413 error code (invalid nginx configuration)
         this.on("error", function(file, errorMessage, xhr) {
             if (xhr && xhr.status === 413) {
-                showError(file, "File too large to upload. If you are using a reverse proxy, make sure that the allowed body size is at least 50MB.");
+                showError(file, "File too large to upload. If you are using a reverse proxy, make sure that the allowed body size is at least 70MB.");
             } else {
                 showError(file, "Server responded with code " + xhr.status);
             }
