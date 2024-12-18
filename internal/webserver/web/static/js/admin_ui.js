e4fe1452fbb679df648243e33d8ed95b43f4d049
Marc Ole Bulling <Marc-Ole@gmx.de>
2024-12-18 12:02:21 +0100

    Add new API keys to UI without reloading, add small animation, remove html_users.tmpl originaly from unpublished branch

--- internal/webserver/web/static/js/admin_ui.js
+++ internal/webserver/web/static/js/admin_ui.js
@@ -278,52 +278,6 @@ function requestFileInfo(fileId, uid) {
 }
 
 
-function addFriendlyNameChange() {
-    document.querySelectorAll(".apiname").forEach(function(node) {
-        node.onclick = function() {
-            if (this.classList.contains("isBeingEdited"))
-                return;
-            this.classList.add("isBeingEdited");
-            var val = this.innerHTML;
-            var input = document.createElement("input");
-            input.size = 5;
-            input.value = val;
-            let row = this;
-            let allowEdit = true;
-
-            let submitEntry = function() {
-                if (!allowEdit)
-                    return;
-                allowEdit = false;
-                var newName = input.value;
-                input.parentNode.innerHTML = newName;
-
-                row.classList.remove("isBeingEdited");
-
-                apiAuthFriendlyName(row.id, newName)
-                    .catch(error => {
-                        alert("Unable to save name: " + error);
-                        console.error('Error:', error);
-                    });
-            };
-
-            input.onblur = submitEntry;
-            input.addEventListener("keyup", function(event) {
-                // Enter
-                if (event.keyCode === 13) {
-                    event.preventDefault();
-                    submitEntry();
-                }
-            });
-            this.innerHTML = "";
-            this.appendChild(input);
-            input.focus();
-        };
-    });
-}
-
-
-
 function parseProgressStatus(eventData) {
     let container = document.getElementById(`us-container-${eventData.chunk_id}`);
     if (container == null) {
@@ -601,17 +555,109 @@ function deleteApiKey(apiKey) {
 
 
 function newApiKey() {
-
     document.getElementById("button-newapi").disabled = true;
-
     apiAuthCreate()
         .then(data => {
-            location.reload();
+            addRowApi(data.Id);
+            document.getElementById("button-newapi").disabled = false;
         })
         .catch(error => {
             alert("Unable to create API key: " + error);
             console.error('Error:', error);
         });
+}
+
+
+
+
+function addFriendlyNameChange(apiKey) {
+    let cell = document.getElementById("friendlyname-" + apiKey);
+    if (cell.classList.contains("isBeingEdited"))
+        return;
+    cell.classList.add("isBeingEdited");
+    let currentName = cell.innerHTML;
+    let input = document.createElement("input");
+    input.size = 5;
+    input.value = currentName;
+    let allowEdit = true;
+
+    let submitEntry = function() {
+        if (!allowEdit)
+            return;
+        allowEdit = false;
+        let newName = input.value;
+        cell.innerHTML = newName;
+
+        cell.classList.remove("isBeingEdited");
+
+        apiAuthFriendlyName(apiKey, newName)
+            .catch(error => {
+                alert("Unable to save name: " + error);
+                console.error('Error:', error);
+            });
+    };
+
+    input.onblur = submitEntry;
+    input.addEventListener("keyup", function(event) {
+        // Enter
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            submitEntry();
+        }
+    });
+    cell.innerHTML = "";
+    cell.appendChild(input);
+    input.focus();
+}
+
+
+
+
+function addRowApi(apiKey) {
+
+    let table = document.getElementById("apitable");
+    let row = table.insertRow(0);
+    row.id = "row-" + apiKey;
+    let cellFriendlyName = row.insertCell(0);
+    let cellId = row.insertCell(1);
+    let cellLastUsed = row.insertCell(2);
+    let cellPermissions = row.insertCell(3);
+    let cellButtons = row.insertCell(4);
+    let cellEmpty = row.insertCell(5);
+
+    cellFriendlyName.classList.add("newApiKey");
+    cellId.classList.add("newApiKey");
+    cellLastUsed.classList.add("newApiKey");
+    cellPermissions.classList.add("newApiKey");
+    cellButtons.classList.add("newApiKey");
+    cellEmpty.classList.add("newApiKey");
+
+
+    cellFriendlyName.innerText = "Unnamed key";
+    cellFriendlyName.id = "friendlyname-" + apiKey;
+    cellFriendlyName.onclick = function() {
+        addFriendlyNameChange(apiKey);
+    };
+    cellId.innerText = apiKey;
+    cellLastUsed.innerText = "Never";
+    cellButtons.innerHTML = '<button type="button" data-clipboard-text="' + apiKey + '"  onclick="showToast()" title="Copy API Key" class="copyurl btn btn-outline-light btn-sm"><i class="bi bi-copy"></i></button> <button id="delete-' + apiKey + '" type="button" class="btn btn-outline-danger btn-sm" onclick="deleteApiKey(\'' + apiKey + '\')" title="Delete"><i class="bi bi-trash3"></i></button>';
+    cellPermissions.innerHTML = `
+	    	<i id="perm_view_` + apiKey + `" class="bi bi-eye apiperm-granted" title="List Uploads" onclick='changeApiPermission("` + apiKey + `","PERM_VIEW", "perm_view_` + apiKey + `");'></i>
+	    	<i id="perm_upload_` + apiKey + `" class="bi bi-file-earmark-arrow-up apiperm-granted" title="Upload" onclick='changeApiPermission("` + apiKey + `","PERM_UPLOAD", "perm_upload_` + apiKey + `");'></i>
+	    	<i id="perm_edit_` + apiKey + `" class="bi bi-pencil apiperm-granted" title="Edit Uploads" onclick='changeApiPermission("` + apiKey + `","PERM_EDIT", "perm_edit_` + apiKey + `");'></i>
+	    	<i id="perm_delete_` + apiKey + `" class="bi bi-trash3 apiperm-granted" title="Delete Uploads" onclick='changeApiPermission("` + apiKey + `","PERM_DELETE", "perm_delete_` + apiKey + `");'></i>
+	    	<i id="perm_replace_` + apiKey + `" class="bi bi-recycle apiperm-notgranted" title="Replace Uploads" onclick='changeApiPermission("` + apiKey + `","PERM_REPLACE", "perm_replace_` + apiKey + `");'></i>
+	    	<i id="perm_api_` + apiKey + `" class="bi bi-sliders2 apiperm-notgranted" title="Manage API Keys" onclick='changeApiPermission("` + apiKey + `","PERM_API_MOD", "perm_api_` + apiKey + `");'></i>`;
+
+    setTimeout(() => {
+        cellFriendlyName.classList.remove("newApiKey");
+        cellId.classList.remove("newApiKey");
+        cellLastUsed.classList.remove("newApiKey");
+        cellPermissions.classList.remove("newApiKey");
+        cellButtons.classList.remove("newApiKey");
+        cellEmpty.classList.remove("newApiKey");
+    }, 700);
+
 }
 
 
