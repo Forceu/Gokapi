87551b617862df03824bff584488be9590f8fdb1
zwimer <github@zwimer.com>
2025-08-12 02:24:49 -0400

    Update Dockerfile to support UMASK, PUID, PGID

--- dockerentry.sh
+++ dockerentry.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-if [ "$DOCKER_NONROOT" = "true" ]; then
+if [ -n "$UMASK" ]; then
+	umask "$UMASK"
+fi && \
+if [ -n "$PUID$PGID" ] || [ "$DOCKER_NONROOT" = "true" ]; then
 	echo "Setting permissions" && \
+	PUID="${PUID-$(id -u gokapi)}" && \
+	PGID="${PGID-$(id -g gokapi)}" && \
+	sed -E 's/^(gokapi:x):([0-9]+)(.*)$/\1:'"$PGID"'\3/g' -i /etc/group && \
+	sed -E 's/^(gokapi:x):([0-9]+:[0-9]+)(.*)$/\1:'"$PUID:$PGID"'\3/' -i /etc/passwd && \
 	chown -R gokapi:gokapi /app && \
 	chmod -R 700 /app && \
 	echo "Starting application" && \
