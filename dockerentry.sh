a46ef9007170fd5df62b1b8857916f7f15470c60
Spaghetti <spaghetti.coder.ru@gmail.com>
2025-10-19 19:58:20 +0300

    deprecate DOCKER_NONROOT in favor of docker --user ...
    * remove redundant named volumes creation in compose
    * remove DOCKER_NONROOT from demo env file
    * add deprecation warning to docker entrypoint
    * update documentation

--- dockerentry.sh
+++ dockerentry.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 if [ "$DOCKER_NONROOT" = "true" ]; then
+	# TODO for the next major upgrade version:
+	# 	- Remove this code block and leave only exec /app/gokapi "@"
+	# 	- Remove gokapi user / group creation in Dockerfile
+	# 	- Remove su-exec installation from the Dockerfile
+
+	echo "WARNING:"
+	echo "	Usage of DOCKER_NONROOT is deprecated in favor of docker --user option!"
+	echo "	Please visit the Gokapi docker setup documentation page for more details."
+	echo
 	echo "Setting permissions" && \
 	chown -R gokapi:gokapi /app && \
 	chmod -R 700 /app && \
