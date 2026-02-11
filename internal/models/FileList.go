4ea660506ebb866ae5077804c380f7a14647fd2f
Marc Ole Bulling <Marc-Ole@gmx.de>
2026-02-11 22:29:46 +0100

    Replace space with underscore instead of url escaping it when adding filename to URL

--- internal/models/FileList.go
+++ internal/models/FileList.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/jinzhu/copier"
@@ -108,7 +109,7 @@ func (f *File) ToFileApiOutput(serverUrl string, useFilenameInUrl bool) (FileApi
 
 func getDownloadUrl(input FileApiOutput, serverUrl string, useFilename bool) string {
 	if useFilename {
-		return serverUrl + "d/" + input.Id + "/" + url.PathEscape(input.Name)
+		return serverUrl + "d/" + input.Id + "/" + escapeFilename(input.Name)
 	}
 	return serverUrl + "d?id=" + input.Id
 }
@@ -121,9 +122,14 @@ func getHotlinkUrl(input FileApiOutput, serverUrl string, useFilename bool) stri
 		return serverUrl + "h/" + input.HotlinkId
 	}
 	if useFilename {
-		return serverUrl + "dh/" + input.Id + "/" + url.PathEscape(input.Name)
+		return serverUrl + "dh/" + input.Id + "/" + escapeFilename(input.Name)
 	}
 	return serverUrl + "downloadFile?id=" + input.Id
+}
+
+// escapeFilename does a regular url escape, but replaces spaces with underscores for better readability
+func escapeFilename(filename string) string {
+	return url.PathEscape(strings.Replace(filename, " ", "_", -1))
 }
 
 // ToJsonResult converts the file info to a json String used for returning a result for an upload
